70801424b24b42cb893734fc820898179ec90ad6
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2023-03-09 21:23:00 +0530

    Add playRound function to check who win

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const max = 100;
 // When user enter the choice
-const playerSelection = prompt("What do you choice? ").toUpperCase();
+const playerSelection = prompt("What do you choose? ").toUpperCase();
+const computerSelection = getComputerChoice();
 // Computer choice 
 function getComputerChoice () {
     const num = Math.floor(Math.random () * 100);
@@ -14,6 +15,24 @@ function getComputerChoice () {
     }
 }
 
-
-console.log(userChoice);
-console.log(getComputerChoice ());
+// Check who is win
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+    return ("You Lose! Paper cover Rock");
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        return ("You Lose! Scissors cuts Paper");
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        return ("You Lose! Rock break Scissors");
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        return ("You Win! Rock break Scissors");
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return ("You Win! Paper covers Rock");
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        return ("You Win! Scissors cuts Paper");
+    } else {
+        return ("Tie! No one Win")
+    }
+} 
+console.log(playRound(playerSelection, computerSelection));
+console.log(playerSelection);
+console.log(computerSelection);
